957468e802352fe12380a54a0be154b3722b9763
Michaël Landry <74638031+lethargiesleeps@users.noreply.github.com>
2022-06-13 12:42:35 -0400

    log-analysis + readme update

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Exercism
+# Exercism Solutions
+
+This repo just hosts my solutions for Exercism exercises. I'm currently working on the C# track,  
+and the solutions can be found in each problem's .cs file.  
+The test files contain all the tests to be passed for the problem to be considered completed.  
+I'll be commiting to this repo every time I pass a problem and am hoping to solve one every 2-3 days.
+
+## About Exercism
+
+Exercism is an awesome, awesome site where you can download coding challenges in a variety of programming languages the just help reinforce your problem solving skills and language mastery.  
+They have their built in text editor for solving the problems, but I recommend using the CLI tool they have to work in your IDE.
+
+## Links
+
+Check out [Exercism](https://exercism.org/), it's a great place!
+[My profile](https://exercism.org/profiles/lethargiesleeps)  
+
+## Reach out
+
+If you are solving some problems, and need a hand, reach out! I'm hoping to mentor on the platform once I have more solutions published.
+
--- csharp/log-analysis/LogAnalysis.cs
+++ csharp/log-analysis/LogAnalysis.cs
@@ -1,12 +1,45 @@
 using System;
+using System.Text;
 
-public static class LogAnalysis 
+public static class LogAnalysis
 {
+    private static string test = "hello".SubstringAfter("hello");
+
     // TODO: define the 'SubstringAfter()' extension method on the `string` type
+    public static string SubstringAfter(this string str, string message)
+    {
+        var strings = str.Split(message);
+        return strings[1];
+    }
 
     // TODO: define the 'SubstringBetween()' extension method on the `string` type
-    
+    public static string SubstringBetween(this string str, string firstDel, string lastDel)
+    {
+        var firstSplit = str.Split(firstDel);
+        var secondSplit = firstSplit[1].Split(lastDel);
+        return secondSplit[0];
+    }
+
     // TODO: define the 'Message()' extension method on the `string` type
+    public static string Message(this string str)
+    {
+        var strings = str.Split("]: ");
+        return strings[1];
+    }
 
     // TODO: define the 'LogLevel()' extension method on the `string` type
+    public static string LogLevel(this string str)
+    {
+        var strings = str.Split(':');
+        var logLevel = new StringBuilder();
+        foreach (var c in strings[0])
+        {
+            if (!c.Equals('[') && !c.Equals(']'))
+                logLevel.Append(c);
+        }
+
+        return logLevel.ToString();
+    }
 }
+
+    
